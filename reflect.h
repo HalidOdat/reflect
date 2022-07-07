ab941420553179df05b2e155af33b02d2c16f0af
HalidOdat <halidodat@gmail.com>
2022-07-07 14:50:09 +0200

    Implement single character punctuators

--- reflect.h
+++ reflect.h
@@ -7,6 +7,30 @@
 typedef enum ReflectTokenType {
   REFLECT_TOKEN_EOF,
   REFLECT_TOKEN_INTEGER,
+  REFLECT_TOKEN_LBRACKET,
+  REFLECT_TOKEN_RBRACKET,
+  REFLECT_TOKEN_LPAREN,
+  REFLECT_TOKEN_RPAREN,
+  REFLECT_TOKEN_LBRACE,
+  REFLECT_TOKEN_RBRACE,
+  REFLECT_TOKEN_DOT,
+  REFLECT_TOKEN_AMPERSAND,
+  REFLECT_TOKEN_STAR,
+  REFLECT_TOKEN_PLUS,
+  REFLECT_TOKEN_MINUS,
+  REFLECT_TOKEN_TILDE,
+  REFLECT_TOKEN_EXCLAMATION,
+  REFLECT_TOKEN_SLASH,
+  REFLECT_TOKEN_PERCENT,
+  REFLECT_TOKEN_LESS,
+  REFLECT_TOKEN_GREATER,
+  REFLECT_TOKEN_CARET,
+  REFLECT_TOKEN_PIPE,
+  REFLECT_TOKEN_QUESTION,
+  REFLECT_TOKEN_COLON,
+  REFLECT_TOKEN_SEMICOLON,
+  REFLECT_TOKEN_EQUAL,
+  REFLECT_TOKEN_HASH,
 } ReflectTokenType;
 
 typedef struct ReflectToken {
@@ -47,6 +71,10 @@ static char _reflect_lexer_char_current(ReflectLexer* lexer) {
   return *lexer->stream;
 }
 
+static char _reflect_lexer_char_advance(ReflectLexer* lexer) {
+  ++lexer->stream;
+}
+
 static char _reflect_lexer_char_next(ReflectLexer* lexer) {
   char c = *lexer->stream;
   lexer->stream++;
@@ -60,26 +88,61 @@ static bool _reflect_lexer_integer_lex(ReflectLexer* lexer, uint64_t* out_result
     result = result * 10 + (c - '0'); 
   }
   *out_result = result;
-  
+
   return true;
 }
 
+
 REFLECT_API bool reflect_lexer_token_next(ReflectLexer* lexer, ReflectToken* token) {
+
+#define _REFLECT_LEXER_CASE1(c, t)   \
+  case (c):                          \
+    token->type = (t);               \
+    _reflect_lexer_char_advance(lexer); \
+    return true
+
 _reflect_lexer_again:
   switch (_reflect_lexer_char_current(lexer)) {
     case '\0':
       token->type = REFLECT_TOKEN_EOF;
       return true;
     case ' ':
-      _reflect_lexer_char_current(lexer);
+      _reflect_lexer_char_advance(lexer);
       goto _reflect_lexer_again;
     case '0': case '1': case '2': case '3': case '4':
     case '5': case '6': case '7': case '8': case '9':
       token->type = REFLECT_TOKEN_INTEGER;
       return _reflect_lexer_integer_lex(lexer, &token->integer);
+
+    _REFLECT_LEXER_CASE1('[', REFLECT_TOKEN_LBRACKET);
+    _REFLECT_LEXER_CASE1(']', REFLECT_TOKEN_RBRACKET);
+    _REFLECT_LEXER_CASE1('(', REFLECT_TOKEN_LPAREN);
+    _REFLECT_LEXER_CASE1(')', REFLECT_TOKEN_RPAREN);
+    _REFLECT_LEXER_CASE1('{', REFLECT_TOKEN_LBRACE);
+    _REFLECT_LEXER_CASE1('}', REFLECT_TOKEN_RBRACE);
+    _REFLECT_LEXER_CASE1('.', REFLECT_TOKEN_DOT);
+    _REFLECT_LEXER_CASE1('&', REFLECT_TOKEN_AMPERSAND);
+    _REFLECT_LEXER_CASE1('*', REFLECT_TOKEN_STAR);
+    _REFLECT_LEXER_CASE1('+', REFLECT_TOKEN_PLUS);
+    _REFLECT_LEXER_CASE1('-', REFLECT_TOKEN_MINUS);
+    _REFLECT_LEXER_CASE1('~', REFLECT_TOKEN_TILDE);
+    _REFLECT_LEXER_CASE1('!', REFLECT_TOKEN_EXCLAMATION);
+    _REFLECT_LEXER_CASE1('/', REFLECT_TOKEN_SLASH);
+    _REFLECT_LEXER_CASE1('%', REFLECT_TOKEN_PERCENT);
+    _REFLECT_LEXER_CASE1('<', REFLECT_TOKEN_LESS);
+    _REFLECT_LEXER_CASE1('>', REFLECT_TOKEN_GREATER);
+    _REFLECT_LEXER_CASE1('^', REFLECT_TOKEN_CARET);
+    _REFLECT_LEXER_CASE1('|', REFLECT_TOKEN_PIPE);
+    _REFLECT_LEXER_CASE1('?', REFLECT_TOKEN_QUESTION);
+    _REFLECT_LEXER_CASE1(':', REFLECT_TOKEN_COLON);
+    _REFLECT_LEXER_CASE1(';', REFLECT_TOKEN_SEMICOLON);
+    _REFLECT_LEXER_CASE1('=', REFLECT_TOKEN_EQUAL);
+    _REFLECT_LEXER_CASE1('#', REFLECT_TOKEN_HASH);
     default:
       return false;
   }
+
+  #undef _REFLECT_LEXER_CASE1
 }
 
 #endif
--- tests/lexer.c
+++ tests/lexer.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 #include <stdbool.h>
 
 #define REFLECT_IMPLEMENTATION
 #include "../reflect.h"
+
+static bool silent = true;
 
 void lexer_test(const char* test_name, const char* source, ReflectToken test_cases[]) {
   printf("  Running Test: %s\n", test_name);
@@ -14,25 +17,55 @@ void lexer_test(const char* test_name, const char* source, ReflectToken test_cas
   reflect_lexer_init(&lexer, source);
   int test_case_number = 1;
   while (test_cases->type != REFLECT_TOKEN_EOF) {
-    printf("    Assertion #%d: ", test_case_number);
-
     if (!reflect_lexer_token_next(&lexer, &token)) {
-      printf("FAILED - could not lex\n");
+      printf("    Assertion #%d: FAILED - could not lexn", test_case_number);
       return;
     }
 
     if (test_cases->type != token.type) {
       // TODO: Display type name.
-      printf("FAILED - type mismatch\n");
+      printf("    Assertion #%d: FAILED - type mismatchn", test_case_number);
       return;
     }
 
     switch (token.type) {
       case REFLECT_TOKEN_INTEGER:
-        printf("PASSED!\n");
+        if (token.integer != test_cases->integer){
+          printf("    Assertion #%d: FAILED - Expected integer %ld, got %ldn", test_case_number, test_cases->integer, token.integer);
+        } else if (!silent) {
+          printf("    Assertion #%d: PASSED!n", test_case_number);
+        }
+        break;
+      case REFLECT_TOKEN_LBRACKET:
+      case REFLECT_TOKEN_RBRACKET:
+      case REFLECT_TOKEN_LPAREN:
+      case REFLECT_TOKEN_RPAREN:
+      case REFLECT_TOKEN_LBRACE:
+      case REFLECT_TOKEN_RBRACE:
+      case REFLECT_TOKEN_DOT:
+      case REFLECT_TOKEN_AMPERSAND:
+      case REFLECT_TOKEN_STAR:
+      case REFLECT_TOKEN_PLUS:
+      case REFLECT_TOKEN_MINUS:
+      case REFLECT_TOKEN_TILDE:
+      case REFLECT_TOKEN_EXCLAMATION:
+      case REFLECT_TOKEN_SLASH:
+      case REFLECT_TOKEN_PERCENT:
+      case REFLECT_TOKEN_LESS:
+      case REFLECT_TOKEN_GREATER:
+      case REFLECT_TOKEN_CARET:
+      case REFLECT_TOKEN_PIPE:
+      case REFLECT_TOKEN_QUESTION:
+      case REFLECT_TOKEN_COLON:
+      case REFLECT_TOKEN_SEMICOLON:
+      case REFLECT_TOKEN_EQUAL:
+      case REFLECT_TOKEN_HASH:
+        if (!silent) {
+          printf("    Assertion #%d: PASSED\n", test_case_number);
+        }
         break;
       case REFLECT_TOKEN_EOF:
-        assert(false && "Don't test agains EOF - it is checked at the end");
+        assert(false && "Unreachable: This check if in the loop");
         break;
       default:
         assert(false && "Unreachable: Unknown token type");
@@ -41,18 +74,58 @@ void lexer_test(const char* test_name, const char* source, ReflectToken test_cas
     test_case_number++;
     test_cases++;
   }
+
+  if (silent) {
+    printf("    All Test Assertions Passed!\n");
+  }
 }
 
-int main() {
+int main(int argc, const char* argv[]) {
+  if (argc > 1 && strcmp(argv[1], "--verbose")) {
+    silent = false;
+  }
+
   printf("Lexer Tests:\n");
   lexer_test(
-    "integer lexing",
+    "Simple Integer Lexing",
     "1 2 3 100000",
     (ReflectToken[]) {
       { .type = REFLECT_TOKEN_INTEGER, .integer = 1 },
       { .type = REFLECT_TOKEN_INTEGER, .integer = 2 },
       { .type = REFLECT_TOKEN_INTEGER, .integer = 3 },
       { .type = REFLECT_TOKEN_INTEGER, .integer = 100000 },
+      { 0 }
+    }
+  );
+
+  lexer_test(
+    "Single Character Punctuator Lexing",
+    "[ ] ( ) { } . & * + - ~ ! / % < > ^ | ? : ; = #",
+    (ReflectToken[]) {
+      { .type = REFLECT_TOKEN_LBRACKET, },
+      { .type = REFLECT_TOKEN_RBRACKET, },
+      { .type = REFLECT_TOKEN_LPAREN, },
+      { .type = REFLECT_TOKEN_RPAREN, },
+      { .type = REFLECT_TOKEN_LBRACE, },
+      { .type = REFLECT_TOKEN_RBRACE, },
+      { .type = REFLECT_TOKEN_DOT, },
+      { .type = REFLECT_TOKEN_AMPERSAND, },
+      { .type = REFLECT_TOKEN_STAR, },
+      { .type = REFLECT_TOKEN_PLUS, },
+      { .type = REFLECT_TOKEN_MINUS, },
+      { .type = REFLECT_TOKEN_TILDE, },
+      { .type = REFLECT_TOKEN_EXCLAMATION, },
+      { .type = REFLECT_TOKEN_SLASH, },
+      { .type = REFLECT_TOKEN_PERCENT, },
+      { .type = REFLECT_TOKEN_LESS, },
+      { .type = REFLECT_TOKEN_GREATER, },
+      { .type = REFLECT_TOKEN_CARET, },
+      { .type = REFLECT_TOKEN_PIPE, },
+      { .type = REFLECT_TOKEN_QUESTION, },
+      { .type = REFLECT_TOKEN_COLON, },
+      { .type = REFLECT_TOKEN_SEMICOLON, },
+      { .type = REFLECT_TOKEN_EQUAL, },
+      { .type = REFLECT_TOKEN_HASH, },
       { 0 }
     }
   );
