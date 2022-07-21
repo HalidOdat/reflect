728b9f05367215bf4ea59d68f650307a61d78e0b
HalidOdat <halidodat@gmail.com>
2022-07-21 09:03:55 +0200

    Better naming for helper functions and macros

--- reflect.h
+++ reflect.h
@@ -1,5 +1,5 @@
-#ifndef _REFLECT_H_
-#define _REFLECT_H_
+#ifndef reflect__H_
+#define reflect__H_
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -189,24 +189,24 @@ REFLECT_API const char* reflect_lexer_error_string_get(ReflectLexer* lexer) {
   return lexer->error_string;
 }
 
-static char _reflect_lexer_char_current(ReflectLexer* lexer) {
+static char reflect__lexer_char_current(ReflectLexer* lexer) {
   return *lexer->stream;
 }
 
-static void _reflect_lexer_char_advance(ReflectLexer* lexer) {
+static void reflect__lexer_char_advance(ReflectLexer* lexer) {
   ++lexer->location.column;
   ++lexer->stream;
 }
 
-static bool _reflect_lexer_char_next_if(ReflectLexer* lexer, const char c) {
-  if (_reflect_lexer_char_current(lexer) == c) {
-    _reflect_lexer_char_advance(lexer);
+static bool reflect__lexer_char_next_if(ReflectLexer* lexer, const char c) {
+  if (reflect__lexer_char_current(lexer) == c) {
+    reflect__lexer_char_advance(lexer);
     return true;
   }
   return false;
 }
 
-static bool _reflect_is_digit_with_radix(const char c, uint8_t radix) {
+static bool reflect__is_digit_with_radix(const char c, uint8_t radix) {
   switch (radix) {
     case 2:  return c == '0' || c == '1';
     case 8:  return c >= '0' && c <= '7';
@@ -217,8 +217,8 @@ static bool _reflect_is_digit_with_radix(const char c, uint8_t radix) {
   assert(false && "Unreachable");
 }
 
-static bool _reflect_char_to_digit_with_radix(const char c, const uint8_t radix, uint8_t* digit) {
-  if (!_reflect_is_digit_with_radix(c, radix)) {
+static bool reflect__char_to_digit_with_radix(const char c, const uint8_t radix, uint8_t* digit) {
+  if (!reflect__is_digit_with_radix(c, radix)) {
     return false;
   }
 
@@ -245,7 +245,7 @@ static bool _reflect_char_to_digit_with_radix(const char c, const uint8_t radix,
   return true;
 }
 
-static const char* _reflect_integer_radix_name(uint8_t radix) {
+static const char* reflect__integer_radix_name(uint8_t radix) {
   switch (radix) {
     case 2:  return "binary";
     case 10: return "decimal";
@@ -255,48 +255,48 @@ static const char* _reflect_integer_radix_name(uint8_t radix) {
   assert(false && "Unreachable");
 }
 
-static bool _reflect_lexer_current_char_to_digit_with_radix(ReflectLexer* lexer, uint8_t radix, uint8_t* digit) {
-  if (_reflect_char_to_digit_with_radix(_reflect_lexer_char_current(lexer), radix, digit)) {
+static bool reflect__lexer_current_char_to_digit_with_radix(ReflectLexer* lexer, uint8_t radix, uint8_t* digit) {
+  if (reflect__char_to_digit_with_radix(reflect__lexer_char_current(lexer), radix, digit)) {
     return true;
   }
   return false;
 }
 
-static bool _reflect_lexer_integer_lex(ReflectLexer* lexer, uint64_t* out_result) {
+static bool reflect__lexer_integer_lex(ReflectLexer* lexer, uint64_t* out_result) {
   uint64_t result = 0;
   uint8_t  radix   = 10;
 
   // Check for radix specification.
-  if (_reflect_lexer_char_current(lexer) == '0') {
-    _reflect_lexer_char_advance(lexer);
+  if (reflect__lexer_char_current(lexer) == '0') {
+    reflect__lexer_char_advance(lexer);
 
-    if (tolower(_reflect_lexer_char_current(lexer)) == 'x') {
-      _reflect_lexer_char_advance(lexer);
+    if (tolower(reflect__lexer_char_current(lexer)) == 'x') {
+      reflect__lexer_char_advance(lexer);
       
       radix = 16;
-      if (!_reflect_is_digit_with_radix(_reflect_lexer_char_current(lexer), radix)) {
+      if (!reflect__is_digit_with_radix(reflect__lexer_char_current(lexer), radix)) {
         lexer->error_code = REFLECT_ERROR_INVALID_INTEGER;
         snprintf(
           lexer->error_string,
           REFLECT_LEXER_ERROR_STRING_MAX_LENGTH,
           "invalid char \"%c\" in %s integer constant",
-          _reflect_lexer_char_current(lexer),
-          _reflect_integer_radix_name(radix)
+          reflect__lexer_char_current(lexer),
+          reflect__integer_radix_name(radix)
         );
 
         // Skip over, for error handling
-        _reflect_lexer_char_advance(lexer);
+        reflect__lexer_char_advance(lexer);
         return false;
       }
-    } else if (_reflect_is_digit_with_radix(_reflect_lexer_char_current(lexer), 8)) {
+    } else if (reflect__is_digit_with_radix(reflect__lexer_char_current(lexer), 8)) {
       radix = 8;
     }
   }
 
   uint8_t digit;
-  while (_reflect_lexer_current_char_to_digit_with_radix(lexer, radix, &digit)) {
+  while (reflect__lexer_current_char_to_digit_with_radix(lexer, radix, &digit)) {
     result = result * (uint64_t)radix + (uint64_t)digit;
-    _reflect_lexer_char_advance(lexer);
+    reflect__lexer_char_advance(lexer);
   }
 
   // TODO: Handle suffix
@@ -307,36 +307,36 @@ static bool _reflect_lexer_integer_lex(ReflectLexer* lexer, uint64_t* out_result
 
 REFLECT_API bool reflect_lexer_token_next(ReflectLexer* lexer, ReflectToken* token) {
 
-#define _REFLECT_LEXER_CASE1(c, t)      \
+#define REFLECT__LEXER_CASE1(c, t)      \
   case (c):                             \
     token->type = (t);                  \
-    _reflect_lexer_char_advance(lexer); \
+    reflect__lexer_char_advance(lexer); \
     return true
 
-#define _REFLECT_LEXER_CASE2(c1, t1, c2, t2)        \
+#define REFLECT__LEXER_CASE2(c1, t1, c2, t2)        \
   case (c1):                                        \
     token->type = (t1);                             \
-    _reflect_lexer_char_advance(lexer);             \
-    if (_reflect_lexer_char_next_if(lexer, (c2))) { \
+    reflect__lexer_char_advance(lexer);             \
+    if (reflect__lexer_char_next_if(lexer, (c2))) { \
       token->type = (t2);                           \
     }                                               \
     return true
 
-#define _REFLECT_LEXER_CASE3(c1, t1, c2, t2, c3, t3)       \
+#define REFLECT__LEXER_CASE3(c1, t1, c2, t2, c3, t3)       \
   case (c1):                                               \
     token->type = (t1);                                    \
-    _reflect_lexer_char_advance(lexer);                    \
-    if (_reflect_lexer_char_next_if(lexer, (c2))) {        \
+    reflect__lexer_char_advance(lexer);                    \
+    if (reflect__lexer_char_next_if(lexer, (c2))) {        \
       token->type = (t2);                                  \
-    } else if (_reflect_lexer_char_next_if(lexer, (c3))) { \
+    } else if (reflect__lexer_char_next_if(lexer, (c3))) { \
       token->type = (t3);                                  \
     }                                                      \
     return true
 
 
-_reflect_lexer_again:
+reflect__lexer_again:
   token->location = lexer->location;
-  switch (_reflect_lexer_char_current(lexer)) {
+  switch (reflect__lexer_char_current(lexer)) {
     case '\0':
       token->type = REFLECT_TOKEN_EOF;
       return true;
@@ -346,74 +346,74 @@ _reflect_lexer_again:
 
       // fallthrough
     case ' ':
-      _reflect_lexer_char_advance(lexer);
-      goto _reflect_lexer_again;
+      reflect__lexer_char_advance(lexer);
+      goto reflect__lexer_again;
     case '0': case '1': case '2': case '3': case '4':
     case '5': case '6': case '7': case '8': case '9':
       token->type     = REFLECT_TOKEN_INTEGER;
       token->location = lexer->location;
-      return _reflect_lexer_integer_lex(lexer, &token->as.integer);
-    _REFLECT_LEXER_CASE1('[', REFLECT_TOKEN_LBRACKET);
-    _REFLECT_LEXER_CASE1(']', REFLECT_TOKEN_RBRACKET);
-    _REFLECT_LEXER_CASE1('(', REFLECT_TOKEN_LPAREN);
-    _REFLECT_LEXER_CASE1(')', REFLECT_TOKEN_RPAREN);
-    _REFLECT_LEXER_CASE1('{', REFLECT_TOKEN_LBRACE);
-    _REFLECT_LEXER_CASE1('}', REFLECT_TOKEN_RBRACE);
-    _REFLECT_LEXER_CASE1(',', REFLECT_TOKEN_COMMA);
-    _REFLECT_LEXER_CASE1('~', REFLECT_TOKEN_TILDE);
-    _REFLECT_LEXER_CASE1('?', REFLECT_TOKEN_QUESTION);
-    _REFLECT_LEXER_CASE1(':', REFLECT_TOKEN_COLON);
-    _REFLECT_LEXER_CASE1(';', REFLECT_TOKEN_SEMICOLON);
-    _REFLECT_LEXER_CASE2('#', REFLECT_TOKEN_HASH,      '#', REFLECT_TOKEN_HASH_HASH);
-    _REFLECT_LEXER_CASE2('^', REFLECT_TOKEN_CARET,     '=', REFLECT_TOKEN_XOR_ASSIGN);
-    _REFLECT_LEXER_CASE2('=', REFLECT_TOKEN_ASSIGN,    '=', REFLECT_TOKEN_EQUALS);
-    _REFLECT_LEXER_CASE2('%', REFLECT_TOKEN_PERCENT,   '=', REFLECT_TOKEN_MOD_ASSIGN);
-    _REFLECT_LEXER_CASE2('!', REFLECT_TOKEN_NOT,       '=', REFLECT_TOKEN_NOT_EQUALS);
-    _REFLECT_LEXER_CASE2('*', REFLECT_TOKEN_STAR,      '=', REFLECT_TOKEN_MUL_ASSIGN);
-    _REFLECT_LEXER_CASE3('&', REFLECT_TOKEN_AMPERSAND, '=', REFLECT_TOKEN_AND_ASSIGN, '&', REFLECT_TOKEN_LOGICAL_AND);
-    _REFLECT_LEXER_CASE3('|', REFLECT_TOKEN_PIPE,      '=', REFLECT_TOKEN_OR_ASSIGN,  '|', REFLECT_TOKEN_LOGICAL_OR);
-    _REFLECT_LEXER_CASE3('+', REFLECT_TOKEN_PLUS,      '=', REFLECT_TOKEN_ADD_ASSIGN, '+', REFLECT_TOKEN_INCREMENT);
+      return reflect__lexer_integer_lex(lexer, &token->as.integer);
+    REFLECT__LEXER_CASE1('[', REFLECT_TOKEN_LBRACKET);
+    REFLECT__LEXER_CASE1(']', REFLECT_TOKEN_RBRACKET);
+    REFLECT__LEXER_CASE1('(', REFLECT_TOKEN_LPAREN);
+    REFLECT__LEXER_CASE1(')', REFLECT_TOKEN_RPAREN);
+    REFLECT__LEXER_CASE1('{', REFLECT_TOKEN_LBRACE);
+    REFLECT__LEXER_CASE1('}', REFLECT_TOKEN_RBRACE);
+    REFLECT__LEXER_CASE1(',', REFLECT_TOKEN_COMMA);
+    REFLECT__LEXER_CASE1('~', REFLECT_TOKEN_TILDE);
+    REFLECT__LEXER_CASE1('?', REFLECT_TOKEN_QUESTION);
+    REFLECT__LEXER_CASE1(':', REFLECT_TOKEN_COLON);
+    REFLECT__LEXER_CASE1(';', REFLECT_TOKEN_SEMICOLON);
+    REFLECT__LEXER_CASE2('#', REFLECT_TOKEN_HASH,      '#', REFLECT_TOKEN_HASH_HASH);
+    REFLECT__LEXER_CASE2('^', REFLECT_TOKEN_CARET,     '=', REFLECT_TOKEN_XOR_ASSIGN);
+    REFLECT__LEXER_CASE2('=', REFLECT_TOKEN_ASSIGN,    '=', REFLECT_TOKEN_EQUALS);
+    REFLECT__LEXER_CASE2('%', REFLECT_TOKEN_PERCENT,   '=', REFLECT_TOKEN_MOD_ASSIGN);
+    REFLECT__LEXER_CASE2('!', REFLECT_TOKEN_NOT,       '=', REFLECT_TOKEN_NOT_EQUALS);
+    REFLECT__LEXER_CASE2('*', REFLECT_TOKEN_STAR,      '=', REFLECT_TOKEN_MUL_ASSIGN);
+    REFLECT__LEXER_CASE3('&', REFLECT_TOKEN_AMPERSAND, '=', REFLECT_TOKEN_AND_ASSIGN, '&', REFLECT_TOKEN_LOGICAL_AND);
+    REFLECT__LEXER_CASE3('|', REFLECT_TOKEN_PIPE,      '=', REFLECT_TOKEN_OR_ASSIGN,  '|', REFLECT_TOKEN_LOGICAL_OR);
+    REFLECT__LEXER_CASE3('+', REFLECT_TOKEN_PLUS,      '=', REFLECT_TOKEN_ADD_ASSIGN, '+', REFLECT_TOKEN_INCREMENT);
     case '.':
       token->type = REFLECT_TOKEN_DOT;
-      _reflect_lexer_char_advance(lexer);
+      reflect__lexer_char_advance(lexer);
       if (lexer->stream[0] == '.' && lexer->stream[1] == '.') {
-        _reflect_lexer_char_advance(lexer);
-        _reflect_lexer_char_advance(lexer);
+        reflect__lexer_char_advance(lexer);
+        reflect__lexer_char_advance(lexer);
         token->type = REFLECT_TOKEN_ELLIPSIS;
       }
       return true;
     case '-':
       token->type = REFLECT_TOKEN_MINUS;
-      _reflect_lexer_char_advance(lexer);
-      if (_reflect_lexer_char_next_if(lexer, '-')) {
+      reflect__lexer_char_advance(lexer);
+      if (reflect__lexer_char_next_if(lexer, '-')) {
         token->type = REFLECT_TOKEN_DECREMENT;
-      } else if (_reflect_lexer_char_next_if(lexer, '=')) {
+      } else if (reflect__lexer_char_next_if(lexer, '=')) {
         token->type = REFLECT_TOKEN_SUB_ASSIGN;
-      } else if (_reflect_lexer_char_next_if(lexer, '>')) {
+      } else if (reflect__lexer_char_next_if(lexer, '>')) {
         token->type = REFLECT_TOKEN_ARROW;
       }
       token->location = lexer->location;
       return true;
     case '/':
       token->type = REFLECT_TOKEN_SLASH;
-      _reflect_lexer_char_advance(lexer);
-      if (_reflect_lexer_char_next_if(lexer, '=')) {
+      reflect__lexer_char_advance(lexer);
+      if (reflect__lexer_char_next_if(lexer, '=')) {
         token->type = REFLECT_TOKEN_DIV_ASSIGN;
       }
       // TODO: Handle comments
-      // else if (_reflect_lexer_char_next_if(lexer, '/')) {
+      // else if (reflect__lexer_char_next_if(lexer, '/')) {
       //   token->type = REFLECT_TOKEN_SUB_ASSIGN;
       // }
       token->location = lexer->location;
       return true;
     case '<':
       token->type = REFLECT_TOKEN_LESS;
-      _reflect_lexer_char_advance(lexer);
-      if (_reflect_lexer_char_next_if(lexer, '=')) {
+      reflect__lexer_char_advance(lexer);
+      if (reflect__lexer_char_next_if(lexer, '=')) {
         token->type = REFLECT_TOKEN_LESS_EQUAL;
-      } else if (_reflect_lexer_char_next_if(lexer, '<')) {
+      } else if (reflect__lexer_char_next_if(lexer, '<')) {
         token->type = REFLECT_TOKEN_LSHIFT;
-        if (_reflect_lexer_char_next_if(lexer, '=')) {
+        if (reflect__lexer_char_next_if(lexer, '=')) {
           token->type = REFLECT_TOKEN_LSHIFT_ASSIGN;
         }
       }
@@ -421,12 +421,12 @@ _reflect_lexer_again:
       return true;
     case '>':
       token->type = REFLECT_TOKEN_GREATER;
-      _reflect_lexer_char_advance(lexer);
-      if (_reflect_lexer_char_next_if(lexer, '=')) {
+      reflect__lexer_char_advance(lexer);
+      if (reflect__lexer_char_next_if(lexer, '=')) {
         token->type = REFLECT_TOKEN_GREATER_EQUAL;
-      } else if (_reflect_lexer_char_next_if(lexer, '>')) {
+      } else if (reflect__lexer_char_next_if(lexer, '>')) {
         token->type = REFLECT_TOKEN_RSHIFT;
-        if (_reflect_lexer_char_next_if(lexer, '=')) {
+        if (reflect__lexer_char_next_if(lexer, '=')) {
           token->type = REFLECT_TOKEN_RSHIFT_ASSIGN;
         }
       }
@@ -438,18 +438,18 @@ _reflect_lexer_again:
         lexer->error_string,
         REFLECT_LEXER_ERROR_STRING_MAX_LENGTH,
         "invalid character '%c'",
-        _reflect_lexer_char_current(lexer)
+        reflect__lexer_char_current(lexer)
       );
       // Skip over invalid character
-      _reflect_lexer_char_advance(lexer);
+      reflect__lexer_char_advance(lexer);
       return false;
   }
 
-  #undef _REFLECT_LEXER_CASE1
-  #undef _REFLECT_LEXER_CASE2
-  #undef _REFLECT_LEXER_CASE3
+  #undef REFLECT__LEXER_CASE1
+  #undef REFLECT__LEXER_CASE2
+  #undef REFLECT__LEXER_CASE3
 }
 
 
 #endif // REFLECT_IMPLEMENTATION
-#endif // _REFLECT_H_
+#endif // reflect__H_
